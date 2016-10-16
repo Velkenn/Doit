659740a0b86a9f20f8c0e36498316b642d533786
Stephen <sdodd987@gmail.com>
2016-10-16 14:13:53 -0500

    added tableview and task class

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Stephen on 10/16/16.
+//  Copyright © 2016 Stephen. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
